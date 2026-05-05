09437ed4d24a3a4702604ab5cf2dd8ce62ea0573
Basampelliadharsh <adharshbasampalli@gmail.com>
2026-05-05 18:24:08 +0530

    adding ts

---
+++ homepage.ts
@@ -0,0 +1,14 @@
+// homepage.ts
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-homepage',
+  standalone: true,
+  imports: [RouterLink],
+  templateUrl: './homepage.html',
+  styleUrl: './homepage.css'
+})
+export class Homepage {
+
+}
